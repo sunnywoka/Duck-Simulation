01b5ddd3039347b2ae6dbacf83477de20f359db8
sunnywoka <yuekaixx@outlook.com>
2023-04-23 13:34:56 +1200

    Add three Quack impletment classes

---
+++ Duck/Mute.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Mute : QuackBehavior {
+    public void quack(){
+        Console.Write("<<Silence>>");
+    }
+
+}
