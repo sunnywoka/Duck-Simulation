a85d009685faa2aa88fa8f6d47415aa02a56d8f6
sunnywoka <yuekaixx@outlook.com>
2023-04-23 13:29:39 +1200

    Add two fly behavior implementation classes

---
+++ Duck/FlyNoWay.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class FlyNoWay : FlyBehavior {
+    public void fly(){
+        Console.Write("I cannot fly");
+    }
+}
